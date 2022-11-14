22f37433c87e39c5bc2881d9a98c40216f75b24f
zageabb <zageabb@users.noreply.github.com>
2022-11-14 12:08:18 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,17 +1,17 @@
 import sqlalchemy as sa
 
-connection_accdb = (
-    r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
-    r"DBQ=.\SAPRoles.accdb;"
-    r"ExtendedAnsiSQL=1;"
-)
-
-#connection_url =(
-#    f'sqlite:///SAPRoles.sqlite'
+#connection_accdb = (
+#    r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
+#    r"DBQ=.\SAPRoles.accdb;"
+#    r"ExtendedAnsiSQL=1;"
 #)
 
-connection_url = sa.engine.URL.create(
-    "access+pyodbc",
-    query={"odbc_connect": connection_accdb}
+connection_url =(
+    f'sqlite:///SAPRoles.db'
 )
+
+#connection_url = sa.engine.URL.create(
+#    "access+pyodbc",
+#    query={"odbc_connect": connection_accdb}
+#)
 engine = sa.create_engine(connection_url)
